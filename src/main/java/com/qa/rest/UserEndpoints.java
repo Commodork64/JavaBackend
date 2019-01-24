c67d9914c1baff85ccb64d61836a41061bdf810a
Calin <the8thnoteofficial@gmail.com>
2019-01-24 16:16:50 +0000

    added CORSFilter, changed update from post to put

--- src/main/java/com/qa/rest/UserEndpoints.java
+++ src/main/java/com/qa/rest/UserEndpoints.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,7 +32,7 @@ public class UserEndpoints {
 	}
 
 	@Path("/updateUser/{id}")
-	@POST
+	@PUT
 	@Produces({ "application/json" })
 	public String updateUser(@PathParam("id") Long id, String user) {
 		return service.updateUser(id, user);
