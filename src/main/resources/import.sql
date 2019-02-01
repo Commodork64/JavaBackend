6c1a92c86774c26014c5371ed78a27746e71f840
Calin <the8thnoteofficial@gmail.com>
2019-02-01 09:45:15 +0000

    minor database changes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,9 @@
 INSERT INTO USER (userid, userName, dateOfBirth, password) VALUES (1, 'Admin', '1970/01/01', 'admin')
+INSERT INTO USER (userid, userName, dateOfBirth, password) VALUES (2, 'HamishDaLegend96', '1996/07/11', 'DonkeyKong4Lyf')
 INSERT INTO USER (userid, userName, dateOfBirth, password) VALUES (3, 'NintendoLover++', '1993/08/12', 'password')
-INSERT INTO USER (userid, userName, dateOfBirth, password) VALUES (2, 'LordofToes', '1987/03/01', 'betterPassword')
+INSERT INTO USER (userid, userName, dateOfBirth, password) VALUES (4, 'Jen-kins?', '1994/02/20', 'Seetalegend')
 
-INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (1, 'Mario 64', '1996/06/23', '3D Platformer', 1)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (1, 'Mario 64', '1996/06/23', '3D Platformer', 1)-
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (2, 'The Legend Of Zelda: Ocarina of Time', '1998/11/21', 'RPG', 1)
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (3, 'Castlevania 64', '1990/01/26', 'Action-Adventure', 1)
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (4, 'Castlevania: Legacy of Darkness', '1999/11/30', 'Action-Adventure', 1)
@@ -11,8 +12,16 @@ INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (6, 'Supe
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (7, 'The Legend Of Zelda: Majora's Mask', '2000/04/27', 'RPG', 1)
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (8, 'Paper Mario', '2000/08/11', 'RPG', 1)
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (9, 'Wipeout 64', '1998/11/10', 'Racing', 1)
-INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (10, 'GoldenEye 007', '1997/08/25', 'FPS', 1)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (10, 'GoldenEye 007', '1997/08/25', 'FPS', 1) -
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (11, 'Banjo-Kazooie', '1998/07/17', '3D Platformer', 1)
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (12, 'Conker's Bad Fur Day', '2001/04/05', '3D Platformer', 1)
-INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (13, 'Donkey Kong 64', '1999/12/03', '3D Platformer', 1)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (13, 'Donkey Kong 64', '1999/12/03', '3D Platformer', 1) -
 INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (14, 'Perfect Dark', '2000/05/22', 'FPS', 1)
+
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (15, 'Donkey Kong 64', '1999/12/03', '3D Platformer', 2)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (16, 'GoldenEye 007', '1997/08/25', 'FPS', 2)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (17, 'Mario 64', '1996/06/23', '3D Platformer', 2)
+
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (18, 'Donkey Kong 64', '1999/12/03', '3D Platformer', 4)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (19, 'GoldenEye 007', '1997/08/25', 'FPS', 4)
+INSERT INTO GAME (gameid, gameName, releaseYear, genre, userid) VALUES (20, 'The Legend Of Zelda: Ocarina of Time', '1998/11/21', 'RPG', 4)
